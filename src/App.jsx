9dadbb4b6b7ecaa13176bedf71c6f6a17f0ae521
Julian <jacaminaur@gmail.com>
2025-02-11 10:05:19 +0100

    CSS and some animations

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import "./App.css";
 import Calculator from "./Components/Calculator/Calculator";
 
 function App() {
-  return (
-    <>
-      <Calculator></Calculator>
-    </>
-  );
+  return <Calculator></Calculator>;
 }
 
 export default App;
--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -1,43 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Calculator.module.css";
 
 function Calculator() {
+  const [switcherStates, setSwitcherStates] = useState([1, 2, 3]);
+  const [switcherState, setSwitcherState] = useState(1);
+
+  function handleSwitcherChange() {
+    const newState = (switcherState % 3) + 1;
+    setSwitcherState(newState);
+  }
+
   return (
-    <div className={styles.calculator}>
-      <div className={styles.firstRow}>
-        <span>calc</span>
+    <div className={styles.container}>
+      <div className={styles.calculator}>
+        <div className={styles.firstRow}>
+          <span className={styles.title}>calc</span>
 
-        <div>
-          <p>Theme</p>
+          <div className={styles.themeSelector}>
+            <p>Theme</p>
 
-          {/* Three buttons with different themes */}
-          <button>Switcher</button>
+            {/* Three buttons with different themes */}
+            <div className={styles.switcher} data-state={switcherState}>
+              <div className={styles.positions}>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+              </div>
+              <div className={styles.button} onClick={handleSwitcherChange}>
+                <span></span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.results}>
-        <input type="text" placeholder="0" />
-      </div>
+        <div className={styles.results}>
+          <span>4,815,162,342</span>
+        </div>
 
-      <div className={styles.buttons}>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className={styles.action}>del</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button className={styles.action}>+</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className={styles.action}>-</button>
-        <button>.</button>
-        <button>0</button>
-        <button className={styles.action}>/</button>
-        <button className={styles.action}>x</button>
-        <button className={styles.action}>Reset</button>
-        <button className={styles.action}>=</button>
+        <div className={styles.buttons}>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className={`${styles.action} ${styles.delete}`}>del</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button className={styles.action}>+</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button className={styles.action}>-</button>
+          <button>.</button>
+          <button>0</button>
+          <button className={styles.action}>/</button>
+          <button className={styles.action}>x</button>
+          <button className={`${styles.action} ${styles.reset}`}>Reset</button>
+          <button className={`${styles.action} ${styles.equal}`}>=</button>
+        </div>
       </div>
     </div>
   );
