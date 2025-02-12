9f1cb3cd5b03f30f46e8267638dd3d1097b73500
Julian <jacaminaur@gmail.com>
2025-02-12 08:30:21 +0100

    Theme Selector

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -1,17 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./Calculator.module.css";
+import { useEffect } from "react";
 
 function Calculator() {
   const [switcherStates, setSwitcherStates] = useState([1, 2, 3]);
   const [switcherState, setSwitcherState] = useState(1);
+  const [result, setResult] = useState("0");
 
   function handleSwitcherChange() {
     const newState = (switcherState % 3) + 1;
     setSwitcherState(newState);
   }
+  useEffect(() => {
+    document
+      .getElementById("container")
+      .classList.remove("theme1", "theme2", "theme3");
+    document.getElementById("container").classList.add(`theme${switcherState}`);
+  }, [switcherState]);
+
+  useEffect(() => {
+    document.getElementById("container").classList.add(`theme${switcherState}`);
+  }, []);
+
+  function handleNumberOrDotAddition(number) {
+    if (result.length > 12) return;
+    setResult(result === "0" ? number : result + number);
+  }
+
+  function handleDeletion() {
+    if (result.length === 1) return setResult("0");
+    setResult(result.slice(0, -1));
+  }
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} theme1`} id="container">
       <div className={styles.calculator}>
         <div className={styles.firstRow}>
           <span className={styles.title}>calc</span>
@@ -34,24 +56,29 @@ function Calculator() {
         </div>
 
         <div className={styles.results}>
-          <span>4,815,162,342</span>
+          <span>{result}</span>
         </div>
 
         <div className={styles.buttons}>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className={`${styles.action} ${styles.delete}`}>del</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
+          <button onClick={() => handleNumberOrDotAddition("7")}>7</button>
+          <button onClick={() => handleNumberOrDotAddition("8")}>8</button>
+          <button onClick={() => handleNumberOrDotAddition("9")}>9</button>
+          <button
+            className={`${styles.action} ${styles.delete}`}
+            onClick={handleDeletion}
+          >
+            del
+          </button>
+          <button onClick={() => handleNumberOrDotAddition("4")}>4</button>
+          <button onClick={() => handleNumberOrDotAddition("5")}>5</button>
+          <button onClick={() => handleNumberOrDotAddition("6")}>6</button>
           <button className={styles.action}>+</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+          <button onClick={() => handleNumberOrDotAddition("1")}>1</button>
+          <button onClick={() => handleNumberOrDotAddition("2")}>2</button>
+          <button onClick={() => handleNumberOrDotAddition("3")}>3</button>
           <button className={styles.action}>-</button>
-          <button>.</button>
-          <button>0</button>
+          <button onClick={() => handleNumberOrDotAddition(".")}>.</button>
+          <button onClick={() => handleNumberOrDotAddition("0")}>0</button>
           <button className={styles.action}>/</button>
           <button className={styles.action}>x</button>
           <button className={`${styles.action} ${styles.reset}`}>Reset</button>
